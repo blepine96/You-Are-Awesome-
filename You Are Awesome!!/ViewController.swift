0046d0937a8fd5d3b33fbcab970595f53dd1f59a
Brandon Lepine <blepine@umass.edu>
2021-09-14 13:52:09 -0400

    I love coding button added in

--- You Are Awesome!!/ViewController.swift
+++ You Are Awesome!!/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("YAY! It loaded")
+        messageLabel.text = "You are not so awesome"
+        
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: Any) {
+        print("The 'Show Message' was pressed")
+        messageLabel.text = "JK You are Awesome"
+        
+    }
+    
+    @IBAction func messageSayItButton(_ sender: Any) {
+        print("👉🏻 the Say it button was pressed!")
+        messageLabel.text = "I'm just going to say it, I dont care if you broke your elbow"
+    }
+    @IBAction func messageHowAmIDoingButton(_ sender: Any) {
+        messageLabel.text = "You are Getting This!"
+    }
+    
+    @IBAction func messageHowDoYouLikeCodingButton(_ sender: Any) {
+        messageLabel.text = "I love coding"
+    }
 }
 
